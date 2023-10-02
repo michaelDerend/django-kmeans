40e147ff786f5bfa3b330c295b29d9748def313d
Dani Kurniawan <47886545+dkzhen@users.noreply.github.com>
2023-10-02 21:13:30 +0700

    Update settings.py

--- appML/settings.py
+++ appML/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*rs52c&@e9xm$%@a=(3mcvg^)&o=(lk+-52h70qaup+89d#m)!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
