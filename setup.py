19c56aeb849962038c1ae68b721586f77e98f0ba
dkzhen <dani314123@gmail.com>
2023-10-25 18:13:23 +0700

    feat: add setup app

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
 import os
-import sys
 import subprocess
+import sys
 
 
 def install_libraries():
     # Baca isi lib.txt untuk mendapatkan daftar library yang diperlukan
-    with open('installed_packages.txt') as f:
-        required_libraries = f.read().splitlines()
+    with open('lib.txt') as f:
+        required_libraries = [line.strip() for line in f if line.strip()]
 
     # Periksa apakah proyek berjalan di dalam virtual environment (venv)
     in_venv = hasattr(os, 'real_prefix') or (
         hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix)
 
     if in_venv:
-        installed_libraries = subprocess.check_output(['pip', 'list']).decode()
-        installed_libraries = [line.split(
-            ' ')[0] for line in installed_libraries.split('\n')[2:]]
-
         # Instalasi library yang diperlukan jika belum terinstal
         for library in required_libraries:
-            if library not in installed_libraries:
-                subprocess.call(['pip', 'install', library])
+            try:
+                subprocess.check_call(['pip', 'install', library])
                 print(f'Library {library} telah diinstal.')
+            except subprocess.CalledProcessError as e:
+                print(f'Error installing {library}: {e}')
     else:
         print('Proyek tidak berjalan dalam virtual environment (venv). Silakan aktifkan venv terlebih dahulu.')
 
